d7f1250471eb1c7d932dc02f36d56c79f06d4b1f
Miguel <mchito@gmail.com>
2025-02-04 13:35:46 +0100

    create health route for mondodb atlas

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   res.json("Hello book readers");
+});
+
+// send ping to mongodb atlas
+router.get("/health", (req, res) => {
+  // send ping to prevent inactivity on mongodb atlas
+  mongoose.connection.db
+    .admin()
+    .ping()
+    .then(() => {
+      res.status(200).json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+      });
+    })
+    .catch((err) => {
+      console.error("MongoDB ping failed:", err);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to connect to MongoDB",
+      });
+    });
 });
 
 module.exports = router;
